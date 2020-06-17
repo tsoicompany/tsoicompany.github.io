7d7fc6d3ffdc3a84ffb0b1373344c2e3e3a2d467
tsoicompany <tsoidaniil.95@gmail.com>
2020-06-17 12:10:04 +0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-$(window).on("load", function () {
+$(window).on('load', function () {
    $("a[rel='mPageScroll2id']").mPageScroll2id();
 
-   $('#header').vide('./video/nature', {
-      bgColor: '#2e4133',
-   });
+   $('#header').vide('./video/nature');
 });
