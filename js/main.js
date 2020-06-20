90e4ec0395ed83f6b079b79a6267a7360ffab5f2
tsoicompany <tsoidaniil.95@gmail.com>
2020-06-20 12:55:19 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
-   $("a[rel='mPageScroll2id']").mPageScroll2id();
+   $("a[rel="mPageScroll2id"]").mPageScroll2id();
 
    $('#header').vide('./video/nature');
 });
